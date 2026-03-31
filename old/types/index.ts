f9ae89ab48e421a86f33232e835b79264f3a08e0
dawi369 <david.erwin.cz68@gmail.com>
2026-03-31 19:07:33 +0200

    rid of old

--- old/types/index.ts
+++
@@ -1,9 +0,0 @@
-// Theme context
-export type Theme = "light" | "dark" | "system";
-
-export interface ThemeContextValue {
-  theme: Theme;
-  is_dark: boolean;
-  set_theme: (theme: Theme) => void;
-  toggle_theme: () => void;
-}
