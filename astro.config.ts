961f61e7b8713a675af84a6d06d0d465a5982b69
dawi369 <david.erwin.cz68@gmail.com>
2026-08-05 13:59:47 -0400

    fix

--- astro.config.ts
+++ astro.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://daviderwin.me',
   output: 'static',
-  trailingSlash: 'always',
   build: {
     format: 'directory',
   },
